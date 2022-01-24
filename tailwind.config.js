0c4a6709c8cb9d03e9fc8105094215564234c2fb
Jameela Nadine Jamena <jamena.nadine@gmail.com>
2022-01-24 16:31:17 +0800

    template done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,js}',
+        './public/**/*.{html,js}',
+        './public/index.html',
+        './public/educ_background.html',
+        '.src/**/*.css',
+        '.public/**/*.css',
+        '.public/images/**/*.{png, jpg, gif}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
